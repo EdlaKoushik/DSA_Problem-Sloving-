15bb443731b8bf67c92a36e3de05285d2ff24a41
EDLA KOUSHIK <koushikedla01@gmail.com>
2025-06-17 18:01:19 +0530

    Update LinearSearch.java

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -11,3 +11,51 @@ public class LinearSearch {
         }
     }
 }
+//linear search numbers
+import java.util.*;
+public class ArraysCC{
+    public static int linearSearch(int numbers[],int key){
+    for(int i=0;i<numbers.length;i++){
+   if(numbers[i]==key){
+   return i;
+   }
+  }
+        return -1;
+    }
+    public static void main(String args[]){
+int numbers[]={2,3,4,6,7,89};
+        int key=10;
+        int index=linearSearch(numbers,key);
+        if(index==-1){
+        System.out.println("key not found");
+        }
+        else{
+            System.out.println("Key found at index ",index);
+        }
+    }
+}
+
+
+//string linear search
+import java.util.*;
+public class StringLinearSearch{
+    public static int linearSearch(String menu[],String key){
+    for(int i=0;i<menu.length;i++){
+   if(menu[i]==key){
+   return i;
+   }
+  }
+        return -1;
+    }
+    public static void main(String args[]){
+String menu[]={"dosa","puri","chapathi","vada","cholebature"};
+        String key="puri";
+        int index=linearSearch(menu,key);
+        if(index==-1){
+        System.out.println("key not found");
+        }
+        else{
+            System.out.println("Key found at index "+index);
+        }
+    }
+}
