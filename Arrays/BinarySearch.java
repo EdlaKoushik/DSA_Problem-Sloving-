ccba47612896b28ee82ae77cfd8cd8a3f69a3050
EDLA KOUSHIK <koushikedla01@gmail.com>
2025-07-03 07:42:16 +0530

    Update BinarySearch.java

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -8,6 +8,37 @@ Time Complexity of the binary search will be O(log n)
          n/2=k
          log n base 2=k
        so Time complexity will be log n--> O(log n)
+
+
+    //"optimized binary search 
+    class Solution {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        int start=0;int end=arr.length-1;
+        int result=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==k){
+                result=mid;
+                end=mid-1;
+                
+            }
+            else if(arr[mid]<k){
+                start =mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return result;
+    
+        }
+        
+    }
+
+
+
+//General binary search
     
 import java.util.*;
 public class Main {
